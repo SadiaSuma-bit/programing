1027db6636c1e19a8447cf985140f67daf6248bc
Sadia Suma <sadiasuma94@outlook.com>
2026-04-22 17:13:59 +0600

    Initial commit

---
+++ Week 2/A2_T7.py
@@ -0,0 +1,16 @@
+# Create a Python program to convert Fahrenheit to Celcius. Round the Celsius to 1 decimal precision.
+
+# Formula to calculate Celcius from Fahrenheit is (Fahrenheit - 32) / 1.8
+
+# Example program run:
+
+# Program starting.
+print("Program starting.")
+# Insert fahrenheits: 50
+Temp = float(input("Insert farrenheits: "))
+Celcius = (Temp - 32) /1.8
+Celcius = round(Celcius, 1)
+# 50.0°F is 10.0°C
+print(f"{Temp}°F is {Celcius}°C")
+# Program ending.
+print("Program ending.")
